a8786f296299eca7ccc742d18248b7ae773f50c0
kb <khushbakht.alizaa@gmail.com>
2025-03-27 16:25:11 +0000

    started

--- sql/user-info.sql
+++ sql/user-info.sql
@@ -1,3 +1,11 @@
 CREATE TABLE Users (
-    User
+    UserID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    UserName VARCHAR(20),
+    Password VARCHAR(20) 
+);
+
+CREATE TABLE User_Statistics(
+    UserID INT NOT NULL,
+    UserName VARCHAR(20),
+    
 )
